d3d0f2dfec90535812847fa37d91cdc614aa53b0
ismailKtami <ktamiismail@gmail.com>
2019-11-21 00:09:15 +0100

    adapter2

--- src/main/java/adapter_head_first_2/Client.java
+++ src/main/java/adapter_head_first_2/Client.java
@@ -1,4 +1,28 @@
 package adapter_head_first_2;
 
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Collections;
 public class Client {
+
+
+    public static void main(String[] args) {
+        List<String> liste= Arrays.asList("hey","hey2","hey3");
+
+        Enumeration enumeration=Collections.enumeration(liste);
+
+        Iterator<String> iterator=new EnumerationIterator(enumeration);
+
+
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+
+
+
+            iterator.remove();
+
+
+    }
 }
--- src/main/java/adapter_head_first_2/EnumerationIterator.java
+++ src/main/java/adapter_head_first_2/EnumerationIterator.java
@@ -1,4 +1,27 @@
 package adapter_head_first_2;
 
-public class EnumerationIterator {
+import java.util.Enumeration;
+import java.util.Iterator;
+
+public class EnumerationIterator implements Iterator {
+    Enumeration enumeration;
+
+    public EnumerationIterator(Enumeration e){
+        this.enumeration=e;
+    }
+    @Override
+    public boolean hasNext() {
+        return enumeration.hasMoreElements();
+    }
+
+    @Override
+    public Object next() {
+        return enumeration.nextElement();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("desoler ne prend pas charge de cette method");
+
+    }
 }
